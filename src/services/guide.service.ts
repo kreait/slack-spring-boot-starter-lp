e4783fb9b718bbc5a10c3b3c53c008b378a9b18d
Ben Denger <ben@kreait.com>
2019-08-23 10:50:55 +0200

    modified urls to point to the real sdk

--- src/services/guide.service.ts
+++ src/services/guide.service.ts
@@ -4,11 +4,11 @@ import axios from 'axios';
 export default {
 
     get() {
-        return axios.get('https://kreait-guide-service.herokuapp.com/guides/BenDenger/TestAsciDoc')
+        return axios.get('https://kreait-guide-service.herokuapp.com/guides/kreait/slack-spring-boot-starter')
             .then((response) => response.data);
     },
     getGuide(id: string) {
-        return axios.get('https://kreait-guide-service.herokuapp.com/guides/BenDenger/TestAsciDoc/' + id)
+        return axios.get('https://kreait-guide-service.herokuapp.com/guides/kreait/slack-spring-boot-starter/' + id)
                 .then((response) => response.data);
         },
 };
